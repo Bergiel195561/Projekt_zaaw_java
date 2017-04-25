c62d5bca3871b25399b8775f3a5bcb4a8447f994
Szabat195727 <195727@edu.p.lodz.pl>
2017-04-25 14:38:55 +0200

    [Stworzenie-klas-struktury-firmy] - Dodanie pracowników i kierowników do firmy i działu

--- staffmanagement/src/test/java/Model/DepartmentTest.java
+++ staffmanagement/src/test/java/Model/DepartmentTest.java
@@ -1,6 +1,7 @@
 package Model;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -8,10 +9,25 @@ import static org.junit.Assert.*;
  * @author krystian
  */
 public class DepartmentTest {
-    
+
+    private Department department;
+
     @Before
     public void setUp() throws Exception {
+        this.department = new Department("Department");
+    }
 
+    @Test
+    public void addMemberToDepartment() {
+        // When
+        OrdinaryEmployee employee = new OrdinaryEmployee("Name", "Surname");
+        int expectedMembersCount = 1;
+
+        // Given
+        department.addDepartmentMember(employee);
+
+        // Then
+        assertEquals(department.getDepartmentMembers().size(), expectedMembersCount);
     }
 
 }
