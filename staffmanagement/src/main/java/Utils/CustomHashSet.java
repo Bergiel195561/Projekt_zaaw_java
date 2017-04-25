496058baf6f1bec7f70ac75aae47fecb4a71c083
Szabat195727 <195727@edu.p.lodz.pl>
2017-04-25 16:13:09 +0200

    [Stworzenie-wlasnego-hashset] - Zmiana hashSet na LinkedHashSet + Testy

--- staffmanagement/src/main/java/Utils/CustomHashSet.java
+++ staffmanagement/src/main/java/Utils/CustomHashSet.java
@@ -2,21 +2,22 @@ package Utils;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 
 /**
  * @author krystian
  */
-public class CustomHashSet<T> extends HashSet<T> {
+public class CustomHashSet<T> extends LinkedHashSet<T> {
 
-    public T get(int index) throws ArrayIndexOutOfBoundsException {
+    public T get(int index) {
         Iterator iterator = this.iterator();
         T elementAtIndex = null;
+        if (index >= this.size() || index < 0){
+            throw new ArrayIndexOutOfBoundsException();
+        }
         int i = 0;
         while (iterator.hasNext() && i <= index) {
             elementAtIndex = (T) iterator.next();
-        }
-        if (elementAtIndex == null){
-            throw new ArrayIndexOutOfBoundsException();
         }
         return elementAtIndex;
     }
