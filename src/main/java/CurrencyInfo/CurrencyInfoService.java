d69737c6d6e1d144c3c18e70a05436a5d6366749
Szabat195727 <195727@edu.p.lodz.pl>
2017-06-12 01:36:09 +0200

    [Pobieranie-walut] - Pobieranie wartości walut względem bazowej waluty

--- src/main/java/CurrencyInfo/CurrencyInfoService.java
+++ src/main/java/CurrencyInfo/CurrencyInfoService.java
@@ -1,5 +1,16 @@
 package CurrencyInfo;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,6 +20,8 @@ import java.util.stream.Collectors;
  * Created by krystian on 12.06.17.
  */
 public class CurrencyInfoService {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
     private final String API_URL = "http://api.fixer.io/";
 
     /**
@@ -43,6 +56,31 @@ public class CurrencyInfoService {
     public void getCurrencyRates(CurrencyType baseType) {
         String url = "latest?base=" + baseType;
         String compareSymbols = generateCurrencySymbols(baseType);
+        try {
+            HttpClient httpClient = HttpClients.createDefault();
+            HttpGet request = new HttpGet(API_URL + url + "&symbols=" + compareSymbols);
+            request.addHeader("ResourceVersion", "v3");
+
+            HttpResponse response = httpClient.execute(request);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                String responseBody = EntityUtils.toString(response.getEntity(), "UTF-8");
+                JsonNode responseJson = objectMapper.readTree(responseBody);
+
+                System.out.println(responseJson);
+
+            } else {
+                System.out.println(
+                        "Oops something went wrong\nHttp response code: " + response.getStatusLine().getStatusCode() + "\nHttp response body: "
+                                + EntityUtils.toString(response.getEntity()));
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Oops something went wrong\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
 }
