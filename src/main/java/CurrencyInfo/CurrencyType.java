0031c9836cfb57b953a57d8cd3bc3da0a372919e
Szabat195727 <195727@edu.p.lodz.pl>
2017-06-12 01:28:36 +0200

    [Pobieranie-walut] - Stworzenie enum z symbolami walut

---
+++ src/main/java/CurrencyInfo/CurrencyType.java
@@ -0,0 +1,9 @@
+package CurrencyInfo;
+
+/**
+ * Klasa przechowująca podstawowe oznaczenia walut światowych
+ * Created by krystian on 12.06.17.
+ */
+public enum CurrencyType {
+    USD, EUR, PLN, GBP, CHF;
+}
