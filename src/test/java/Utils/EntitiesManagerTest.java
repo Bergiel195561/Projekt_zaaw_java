89a375e92460d14f6d2f6d3a7b8ba4555a8c364e
Bergiel195561 <195561@edu.p.lodz.pl>
2017-06-12 20:43:59 +0200

    [Wyszukiwanie po pesel] - zmiana mechanizmu testow, nowy test

--- src/test/java/Utils/EntitiesManagerTest.java
+++ src/test/java/Utils/EntitiesManagerTest.java
@@ -7,6 +7,7 @@ import org.junit.*;
 import java.util.Optional;
 
 import static Utils.EntitiesManager.*;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,26 +18,37 @@ public class EntitiesManagerTest {
 
     private static MongoConnector mongoConnector;
 
+    private static OrdinaryEmployee employeeFirst;
+    private static OrdinaryEmployee employeeSecond;
+    private static OrdinaryEmployee employeeThird;
+    private static String EXPECTED_PESEL = "90080739100";
+    private static String DIFFERENT_PESEL = "90034539100";
+
     @BeforeClass
     public static void  setUp() throws Exception {
         mongoConnector = MongoConnector.getInstance();
+        employeeFirst = new OrdinaryEmployee("Maciej", "Kowalski", "90080739100");
+        employeeSecond = new OrdinaryEmployee("Tomasz", "Zieliński", "91020739098");
+        employeeThird = new OrdinaryEmployee("Jakub", "Madej", "87082039098");
+
+        mongoConnector.getDatastore().save(employeeFirst);
+        mongoConnector.getDatastore().save(employeeSecond);
+        mongoConnector.getDatastore().save(employeeThird);
     }
 
     @AfterClass
     public static void tearDown() throws Exception {
+        mongoConnector.getDatastore().delete(employeeFirst);
+        mongoConnector.getDatastore().delete(employeeSecond);
+        mongoConnector.getDatastore().delete(employeeThird);
+
         mongoConnector.setDbNameForDefault();
     }
 
     @Test
     public void getEmployeesByPeselShouldBeTrue() throws Exception {
         //given
-        String pesel = "90080739100";
-        OrdinaryEmployee employeeFirst = new OrdinaryEmployee("Maciej", "Kowalski", pesel);
-        OrdinaryEmployee employeeSecond = new OrdinaryEmployee("Tomasz", "Zieliński", "91020739098");
-        OrdinaryEmployee employeeThird = new OrdinaryEmployee("Jakub", "Madej", "87082039098");
-        mongoConnector.getDatastore().save(employeeFirst);
-        mongoConnector.getDatastore().save(employeeSecond);
-        mongoConnector.getDatastore().save(employeeThird);
+        String pesel = EXPECTED_PESEL;
 
         //when
         Optional<OrdinaryEmployee> employee = getEmployeeByPesel(pesel);
@@ -49,10 +61,22 @@ public class EntitiesManagerTest {
         assertThat(employee.get()).hasFieldOrPropertyWithValue("surname", "Kowalski")
                 .hasFieldOrPropertyWithValue("pesel", pesel);
 
-        mongoConnector.getDatastore().delete(employeeFirst);
-        mongoConnector.getDatastore().delete(employeeSecond);
-        mongoConnector.getDatastore().delete(employeeThird);
 
+
+    }
+
+    @Test
+    public void getEmployeesByPeselShouldReturnEmptyOptional() throws Exception {
+        //given
+        String pesel = DIFFERENT_PESEL;
+
+        //when
+        Optional<OrdinaryEmployee> employee = getEmployeeByPesel(pesel);
+
+        //then
+        assertFalse(employee.isPresent());
+        assertThat(employee)
+                .isOfAnyClassIn(Optional.class);
     }
 
 }
