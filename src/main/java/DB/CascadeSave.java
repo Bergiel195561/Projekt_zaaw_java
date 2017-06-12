f29ccaa03c349b66ec421fd7f982e77d3270bd94
Szabat195727 <195727@edu.p.lodz.pl>
2017-06-12 20:24:26 +0200

    [Kaskadowy-zapis] - Dodanie JavaDoc do metod

--- src/main/java/DB/CascadeSave.java
+++ src/main/java/DB/CascadeSave.java
@@ -19,8 +19,10 @@ public class CascadeSave {
     }
 
     /**
-     * Kaskadowy zapis firmy
-     * @param company
+     * Kaskadowy zapis firmy wraz ze wszystkimi jej elementami
+     * w odpowiedniej kolejności wymuszonej zależnościami między
+     * elementami firmy
+     * @param company Firma do zapisania w bazie
      */
     public void saveCasdace(Company company) {
         List<Department> departmentList = company.getDepartments();
@@ -39,22 +41,42 @@ public class CascadeSave {
         saveCompany(company);
     }
 
+    /**
+     * Zapis pracowników w bazie
+     * @param employeesForSave Pracownicy do zapisu w bazie
+     */
     private void saveEmployees(CustomHashSet<OrdinaryEmployee> employeesForSave) {
         employeesForSave.forEach(e -> connector.save(e));
     }
 
+    /**
+     * Zapis managera zespołu w bazie
+     * @param managerForSave Manager do zapisu w bazie
+     */
     private void saveManager(Manager managerForSave) {
         connector.save(managerForSave);
     }
 
+    /**
+     * Zapis teamów w bazie
+     * @param teamsForSave Zespoły do zapisu w bazie
+     */
     private void saveTeams(CustomHashSet<Team> teamsForSave) {
         teamsForSave.forEach(e -> connector.save(e));
     }
 
+    /**
+     * Zapis działów w bazie
+     * @param departmentsForSave Lista działów do zapisu w bazie
+     */
     private void saveDepartments(List<Department> departmentsForSave) {
         departmentsForSave.forEach(e -> connector.save(e));
     }
 
+    /**
+     * Zapis firmy w bazie, jeżeli firma istnieje to aktualizujemy
+     * @param company Firma do zapisu w bazie
+     */
     private void saveCompany(Company company){
         CompanyDao companyDao = new CompanyDao(connector.getDatastore());
         Company companyByName = companyDao.getCompanyByName(company.getName());
