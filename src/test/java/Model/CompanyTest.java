f582c71e88e36f86426cafe42b88067c3f9520c9
195739 Włodarczyk Maciej <195739@edu.p.lodz.pl>
2017-06-12 22:14:25 +0200

    Dodanie testow do Company

--- src/test/java/Model/CompanyTest.java
+++ src/test/java/Model/CompanyTest.java
@@ -1,7 +1,11 @@
 package Model;
 
+import com.sun.corba.se.impl.encoding.BufferManagerFactory;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -19,15 +23,126 @@ public class CompanyTest {
 
     @Test
     public void addDepartmentToCompany() {
-        // When
+        //G iven
         Department d = new Department("Department");
         int expectedDepertmentsCount = 1;
 
-        // Given
+        // When
         company.addDepartment(d);
 
         // Then
         assertTrue(company.getDepartments().size() == expectedDepertmentsCount);
     }
+
+    @Test
+    public void setCeo() {
+        // Given
+        Manager m = new Manager();
+        int expectedCeoCount = 1;
+
+        // When
+        company.setCeo(m);
+
+        // Then
+        assertNotNull(company.getCeo());
+    }
+    @Test
+    public void setName(){
+        // Given
+        String name = "CompanyTest";
+
+        // When
+        company.setName(name);
+
+        // Then
+        assertEquals(company.getName(),name);
+    }
+
+    @Test
+    public void setStreet(){
+        // Given
+        String street = "CompanyStreeTest";
+
+        // When
+        company.setStreet(street);
+
+        // Then
+        assertEquals(company.getStreet(),street);
+    }
+
+    @Test
+    public void setCity(){
+        // Given
+        String city = "CompanyCityTest";
+
+        // When
+        company.setCity(city);
+
+        // Then
+        assertEquals(company.getCity(),city);
+    }
+
+    @Test
+    public void setPhone(){
+        // Given
+        String phone = "123456789";
+
+        // When
+        company.setPhone(phone);
+
+        // Then
+        assertEquals(company.getPhone(),phone);
+    }
+
+    @Test
+    public void setDepartments() {
+        // Given
+       List<Department> departments = new ArrayList<>();
+       for (int i=0; i<3; i++){
+           departments.add(new Department());
+       }
+       int expectedSize = 3;
+
+        // When
+        company.setDepartments(departments);
+
+        // Then
+        assertEquals(company.getDepartments().size(),expectedSize);
+        assertTrue(company.getDepartments().equals(departments));
+    }
+
+    public void createCompany(){
+        //Given
+        List<Department> departments = new ArrayList<>();
+        for (int i=0; i<3; i++){
+            departments.add(new Department());
+        }
+
+        //When
+        Company c = new Company("CompanyTest",
+                "CompanyStreetTest",
+                "CompanyCityTest",
+                "123456789",
+                departments);
+
+        //Then
+        assertNotNull(c);
+    }
+
+    @Test
+    public void testtoString(){
+        //Given
+        String expectedResult = "";
+
+        //Then
+        String result = company.toString();
+
+        //When
+        assertEquals(result,expectedResult);
+
+
+    }
+
+
 
 }
