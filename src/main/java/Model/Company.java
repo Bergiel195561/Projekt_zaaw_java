89d1a923576f57ef009dc550fce9141c27578c35
Szabat195727 <195727@edu.p.lodz.pl>
2017-06-12 21:12:48 +0200

    [Kaskadowy-zapis] - Lombok w company, testy do zapisu kaskadowego i unikalności

--- src/main/java/Model/Company.java
+++ src/main/java/Model/Company.java
@@ -1,5 +1,6 @@
 package Model;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.*;
 
@@ -11,6 +12,7 @@ import java.util.List;
  *
  * @author krystian
  */
+@Data
 @Entity(noClassnameStored = true)
 public class Company {
     @Id
@@ -42,61 +44,13 @@ public class Company {
     }
 
     //region Setters
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setDepartments(List<Department> departments) {
-        this.departments = departments;
-    }
-
-    public void setCeo(Manager ceo) {
-        this.ceo = ceo;
-    }
-
     public void addDepartment(Department department) {
         this.departments.add(department);
     }
-
     //endregion
 
 
     //region Getters
-    public String getName() {
-        return name;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public List<Department> getDepartments() {
-        return departments;
-    }
-
-    public Manager getCeo() {
-        return ceo;
-    }
     //endregion
 
 
